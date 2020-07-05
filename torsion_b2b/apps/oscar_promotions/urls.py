8ae8419a340ae7b481e611d825cbeaec353ce276
AlexGr33n <greentr33@gmail.com>
2020-07-05 22:10:47 +0300

    add nav_accounts

--- torsion_b2b/apps/oscar_promotions/urls.py
+++ torsion_b2b/apps/oscar_promotions/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('news/', views.NewsView.as_view(), name='news'),
+    path('news/<slug:slug>/', views.NewsDetailView.as_view(), name='news_detail'),
+    path('about-us/', views.AboutUsView.as_view(), name='about-us'),
+    path('contacts/', views.ContactsView.as_view(), name='contacts'),
 ]
